83ccd7fe1a8844c6fb8dfc73833dcee7ce0e1056
Jonathan Meyer <Jmeyer@swri.org>
2017-05-11 13:01:17 -0500

    Removed build artifacts that pollute the repo space.

--- pcl_plane_fit/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gtest
-# Build directory: /home/mstest9/catkin_ws/src/pcl_plane_fit/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- using_pointclouds/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gtest
-# Build directory: /home/mstest9/catkin_ws/src/using_pointclouds/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
